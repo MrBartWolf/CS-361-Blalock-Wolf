79e38c7228a50e8b5160b9a51017a615a63a2cbc
Anon <supersnackbros8@tutanota.com>
2018-10-05 13:32:18 -0400

    Command line parsing

---
+++ fline.c
@@ -0,0 +1,28 @@
+void main(int argc, char *argv[]) {
+    int iterations
+    int partsMadeByMe
+
+    // Connect to shared memory
+
+    // Connect to semaphores.
+
+    // while (remain > 0) {
+
+        // Determine how many to make and update remain
+
+        // Print "Factory Line %d: Going to make %d parts in %d milliSecs" with myID and data)
+
+        // sleep(myDuration)
+
+        // Create and send production message
+
+        // Increment # iterations
+
+        // Update total # parts made by me
+
+    // }
+
+    // Create and send completion message
+
+    // Clean up IPC.
+}
